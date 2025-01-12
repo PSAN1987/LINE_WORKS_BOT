128e208b091cebdfc4d8ce23f4e11fc74ac33e9c
PSAN1987 <88932722+PSAN1987@users.noreply.github.com>
2025-01-12 12:01:55 +0900

    Add files via upload

--- LINE_WORKS_BOT.py
+++ LINE_WORKS_BOT.py
@@ -199,17 +199,17 @@ search_coordinates_template = [
     {
         "label": "お届け日",
         "variable_name": "delivery_date",
-        "search_area": {"top": 10, "bottom": 50, "left": 20, "right": 200}
+        "search_area": {"top": 1, "bottom": 1, "left": 1, "right": 20}
     },
     {
         "label": "ご使用日",
         "variable_name": "use_date",
-        "search_area": {"top": 15, "bottom": 40, "left": 30, "right": 150}
+        "search_area": {"top": 1, "bottom": 1, "left": 1, "right": 20}
     },
     {
         "label": "学校名",
         "variable_name": "school_name",
-        "search_area": {"top": 10, "bottom": 50, "left": 10, "right": 300}
+        "search_area": {"top": 1, "bottom": 1, "left": 10, "right": 100}
     },
 ]
 
