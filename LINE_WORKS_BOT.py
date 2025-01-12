7045d02fbea16ef89369c491927552a50f3052ac
PSAN1987 <88932722+PSAN1987@users.noreply.github.com>
2025-01-12 23:56:01 +0900

    Add files via upload

--- LINE_WORKS_BOT.py
+++ LINE_WORKS_BOT.py
@@ -199,7 +199,7 @@ search_coordinates_template = [
     {
         "label": "お届け日",
         "variable_name": "delivery_date",
-        "search_area": {"top": 1, "bottom": 20, "left": 0, "right": 20}
+        "search_area": {"top": 1, "bottom": 10, "left": 0, "right": 70}
     },
     {
         "label": "ご使用日",
